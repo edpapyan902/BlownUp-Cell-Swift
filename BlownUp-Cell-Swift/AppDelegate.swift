84ed75593e589c296e3b10c13d93e76f2d14746c
suunyking1219 <sunny.king1219@gmail.com>
2021-04-30 12:01:26 -0700

    ....

--- BlownUp-Cell-Swift/AppDelegate.swift
+++ BlownUp-Cell-Swift/AppDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        
         return true
     }
 
