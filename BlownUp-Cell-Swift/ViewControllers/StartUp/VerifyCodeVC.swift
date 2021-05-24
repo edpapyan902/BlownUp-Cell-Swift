88b952a697de0ca4b4915fd0bf790092f8fea3c6
suunyking1219 <sunny.king1219@gmail.com>
2021-05-24 01:53:26 -0700

    ...

--- BlownUp-Cell-Swift/ViewControllers/StartUp/VerifyCodeVC.swift
+++ BlownUp-Cell-Swift/ViewControllers/StartUp/VerifyCodeVC.swift
@@ -15,17 +15,24 @@ class VerifyCodeVC: BaseVC {
     
     var verify_Timer: Timer? = nil
     
+    @IBOutlet weak var lblResend: UILabel!
     @IBOutlet weak var verifyCodeView: KWVerificationCodeView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initLayout()
+    }
+    
+    func initLayout() {
+        self.lblResend.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.resendCode)))
     }
     
     @IBAction func verifyCode(_ sender: Any) {
         let verifyCode = verifyCodeView.getVerificationCode()
         if verifyCode.count == 6 {
             self.showLoading(self)
-            self.verify_Timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(self.confirmCode), userInfo: nil, repeats: false)
+            self.verify_Timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.confirmCode), userInfo: nil, repeats: false)
         }
     }
     
@@ -39,6 +46,30 @@ class VerifyCodeVC: BaseVC {
             self.gotoResetPasswordVC(email: email)
         } else {
             self.showWarning("Please enter correct code")
+        }
+    }
+    
+    @objc func resendCode() {
+        let params: [String: Any] = [
+            "email": email
+        ]
+        
+        self.showLoading(self)
+        
+        API.instance.forgetPassword(params: params) { (response) in
+            self.hideLoading()
+            
+            if response.error == nil {
+                let forgetPasswordRes: ForgetPasswordRes = response.result.value!
+                if forgetPasswordRes.success {
+                    self.showSuccess(forgetPasswordRes.message)
+                    
+                    self.m_VerifyCode = forgetPasswordRes.data.verify_code
+                    print("verify code", self.m_VerifyCode)
+                } else {
+                    self.showError(forgetPasswordRes.message)
+                }
+            }
         }
     }
 }
